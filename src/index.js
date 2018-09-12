85c95db686e2771672078b7735655faaa5ebb601
Tatsianacs <42011535+Tatsianacs@users.noreply.github.com>
2018-09-12 09:29:54 +0300

    firstrun

--- src/index.js
+++ src/index.js
@@ -3,5 +3,17 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  var count = 0;
+  for (var i = 0; i < preferences.length; i++){
+  var first = preferences[i];
+  var next = preferences[first - 1];
+  var last = preferences[next - 1];
+  console.log(first,next,last);
+  if (last == (i + 1)){
+  
+          count++;
+      }
+
+  }
+  return Math.floor(count/3);
 };
